d72f056c09093a01e0eecc8027ebbac0b04849c2
Bhavik <kapadiab@uoguelph.ca>
2026-09-18 14:49:45 -0400

    Fix string formatting in ValueError message

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
 def validate_columns(df, columns):
     for col in columns:
         if col not in df.columns:
-            raise ValueError(f"Column '{col}' not found in CSV.")
+            raise ValueError("Column '" + col + "' not found in CSV.")
